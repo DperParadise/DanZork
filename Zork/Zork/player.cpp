73778948d6bebcce6210e3e1964fa9fd85a98aa0
Daniel Pérez <dperezparaiso@gmail.com>
2017-03-08 19:35:00 +0100

    Added actions to player

--- Zork/Zork/player.cpp
+++ Zork/Zork/player.cpp
@@ -15,3 +15,33 @@ Player::~Player()
 {
 	RELEASE(location);
 }
+
+void Player::Pickup(Item *item)
+{
+
+}
+
+void Player::Open(Item *item)
+{
+
+}
+
+void Player::Close(Item *item)
+{
+
+}
+
+void Player::LookAt(Entity *entity)
+{
+
+}
+
+void Player::Go(Direction dir)
+{
+
+}
+
+void Player::UseWith(Item *item1, Item *item2)
+{
+
+}
--- Zork/Zork/player.h
+++ Zork/Zork/player.h
@@ -1,13 +1,23 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "globals.h"
+
 class Room;
+class Item;
 class Player : public Entity
 {
 public:
 
 	Player(Room *loc, Type type, const std::string &name, const std::string &desc, std::list<Entity*> list);
 	~Player();
+
+	void Pickup(Item *item);
+	void Open(Item *item);
+	void Close(Item *item);
+	void LookAt(Entity* entity);
+	void Go(Direction);
+	void UseWith(Item *item1, Item *item2);
 
 	Room *location;
 
